9de35b63a0906aad37e30b1ea34d78090e1ef4cd
vcnt72 <vincentsanjaya72@gmail.com>
2021-10-27 16:41:08 +0700

    Update redis initializer

--- redis-init-local.sh
+++ redis-init-local.sh
@@ -2,4 +2,9 @@ wget http://download.redis.io/redis-stable.tar.gz
 tar xvzf redis-stable.tar.gz
 cd redis-stable
 make
+sudo cp src/redis-server /usr/local/bin/
+sudo cp src/redis-cli /usr/local/bin/
+
 redis-server --daemonize yes
+
+rm -rf redis-stable redis-stable.tar.gz
